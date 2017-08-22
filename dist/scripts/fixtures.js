40d3d7b2150e002ecbdf1e71a0298fe9b23e03cb
Luis Camacho <luiscamacho@gmail.com>
2017-08-22 14:32:45 -0400

    Added CollectionCtrl and LandingCtrl - Added album collection to collection template

---
+++ dist/scripts/fixtures.js
@@ -0,0 +1,108 @@
+//Example Album
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtURL: 'assets/images/album_covers/01.png',
+    songs: [
+        {
+          title: 'Blue',
+          duration: '161.71',
+          audioUrl: 'assets/music/blue'
+        },
+        {
+          title: 'Green',
+          duration: '103.96',
+          audioUrl: 'assets/music/green'
+        },
+        {
+          title: 'Red',
+          duration: '268.45',
+          audioUrl: 'assets/music/red'
+        },
+        {
+          title: 'Pink',
+          duration: '153.14',
+          audioUrl: 'assets/music/pink'
+        },
+        {
+          title: 'Magenta',
+          duration: '374.22',
+          audioUrl: 'assets/music/magenta'
+        }
+    ]
+};
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtURL: 'assets/images/album_covers/20.png',
+    songs: [
+        {
+          title: 'Hello Operator?',
+          duration: '4:26'
+        },
+        {
+          title: 'Ring, ring, ring',
+          duration: '3:14'
+        },
+        {
+          title: 'Fits in your pocket',
+          duration: '5:01'
+        },
+        {
+          title: 'Can you hear me now?',
+          duration: '3:21'
+        },
+        {
+          title: 'Wrong phone number',
+          duration: '2:15'
+        },
+        {
+          title: 'Bye Bye',
+          duration: '2:15'
+        },
+        {
+          title: 'Bye Bye',
+          duration: '2:15'
+        }
+    ]
+};
+
+
+var albumCabrera = {
+    title: 'Tigers',
+    artist: 'Homerun man',
+    label: 'Detroit',
+    year: '2003',
+    albumArtURL: 'assets/images/album_covers/15.png',
+    songs: [
+        {
+          title: 'First Base',
+          duration: '4:26'
+        },
+        {
+          title: 'Right Field',
+          duration: '3:14'
+        },
+        {
+          title: 'Home Run',
+          duration: '5:01'
+        },
+        {
+          title: 'Grand Slam',
+          duration: '3:21'
+        },
+        {
+          title: 'Double Play',
+          duration: '2:15'
+        },
+        {
+          title: 'Triple play',
+          duration: '2:15'
+        }
+        ]
+};
