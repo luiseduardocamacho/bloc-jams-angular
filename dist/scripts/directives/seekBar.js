035a854fa82c01c6a2127aa9b84de3a2542ae5c3
Luis Camacho <luiscamacho@gmail.com>
2017-09-01 13:01:36 -0400

    Added thumbStyle methodand applied it to seek_bar template

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -41,14 +41,19 @@
           $document.bind('mousemove.thumb', function(event){
             var percent = calculatePercent(seekBar, event);
             scope.$apply(function(){
-                scope.value = percent * scope,max;
-            });
+                scope.value = percent * scope.max;
+                });
           });
 
           $document.bind('mouseup.thumb', function(){
               $document.unbind('mousemove.thumb');
               $document.unbind('mouseup.thumb');
           });
+
+          scope.thumbStyle = function(){
+            return {left: percentString()};
+          };
+
         };
       }
     }
